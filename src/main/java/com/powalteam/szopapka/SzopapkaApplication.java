1a42fb149458b4dcd8706092d7e77ffc21c77383
Przemysław Kubas <kubasprzemek9@gmail.com>
2025-03-15 00:54:48 +0100

    initializing repository

---
+++ src/main/java/com/powalteam/szopapka/SzopapkaApplication.java
@@ -0,0 +1,13 @@
+package com.powalteam.szopapka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzopapkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzopapkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/powalteam/szopapka/SzopapkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powalteam.szopapka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzopapkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
