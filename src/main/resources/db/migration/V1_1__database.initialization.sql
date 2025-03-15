db20df60bbb53a0650204f832b5fd03828fca074
Przemysław Kubas <kubasprzemek9@gmail.com>
2025-03-15 15:58:11 +0100

    adding database

---
+++ src/main/resources/db/migration/V1_1__database.initialization.sql
@@ -0,0 +1,46 @@
+CREATE TABLE family
+(
+    id SERIAL PRIMARY KEY,
+    family_name VARCHAR(255)
+);
+
+CREATE TABLE users
+(
+  id SERIAL PRIMARY KEY,
+  login VARCHAR(255),
+  password TEXT NOT NULL,
+  name VARCHAR(255),
+  surname VARCHAR(255)
+);
+
+CREATE TABLE status
+(
+    id SERIAL PRIMARY KEY,
+    status VARCHAR(50) NOT NULL CHECK (status IN ('DONE', 'DURING', 'NEW'))
+);
+
+CREATE TABLE user_in_family
+(
+    id SERIAL PRIMARY KEY,
+    id_user BIGINT NOT NULL,
+    id_family BIGINT NOT NULL,
+
+    FOREIGN KEY (id_user) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_family) REFERENCES family (id) ON DELETE CASCADE
+
+);
+
+CREATE TABLE shopping
+(
+    id SERIAL PRIMARY KEY,
+    id_user BIGINT,
+    id_family BIGINT NOT NULL,
+    date DATE,
+    content TEXT,
+    id_status BIGINT NOT NULL,
+
+    FOREIGN KEY (id_user) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_family) REFERENCES family (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_status) REFERENCES status (id) ON DELETE CASCADE
+);
+
