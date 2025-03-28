9f4ac2b4e5847db09b3f93f831dff43456e4f46d
Piotr Kowal <127453125+Powal504@users.noreply.github.com>
2025-03-28 18:21:24 +0100

    Update V1_3__family_members.sql

--- src/main/resources/db/migration/V1_3__family_members.sql
+++ src/main/resources/db/migration/V1_3__family_members.sql
@@ -1,3 +1,7 @@
+ALTER TABLE shopping DROP COLUMN date;
+
+ALTER TABLE shopping ADD COLUMN quantity INTEGER NOT NULL;
+
 CREATE VIEW family_members AS
 SELECT f.family_name, f.image,u.mail, f.family_code
 FROM user_in_family uif
