07aab2eab4eaf11941d583ff9b597604967df968
Przemek Kubas <110121115+JustPrzemek@users.noreply.github.com>
2025-04-02 11:10:17 +0200

    Update SecurityConfig.java

--- src/main/java/com/powalteam/szopapka/security/SecurityConfig.java
+++ src/main/java/com/powalteam/szopapka/security/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -39,7 +41,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://szopapka-frontend.azurewebsites.net"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
