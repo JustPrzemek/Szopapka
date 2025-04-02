8febadec31c78daa4d81673b5cbec52717c57597
Przemek Kubas <110121115+JustPrzemek@users.noreply.github.com>
2025-04-02 10:34:36 +0200

    Update CorsConfig.java

--- src/main/java/com/powalteam/szopapka/configuration/CorsConfig.java
+++ src/main/java/com/powalteam/szopapka/configuration/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173") // Adres Twojego frontendu (lub "*" dla testów)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Tylko potrzebne metody
-            .allowedHeaders("*") // Dopuszcza nagłówki np. "Authorization", "Content-Type"
-            .allowCredentials(true); // Wymagane, jeśli używasz ciasteczek/tokenów
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
