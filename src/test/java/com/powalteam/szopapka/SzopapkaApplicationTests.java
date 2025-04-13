5866f834e39f8d151eeb01789be8d9ce8b49186e
Przemysław Kubas <kubasprzemek9@gmail.com>
2025-04-13 14:45:00 +0200

    disable

--- src/test/java/com/powalteam/szopapka/SzopapkaApplicationTests.java
+++ src/test/java/com/powalteam/szopapka/SzopapkaApplicationTests.java
@@ -1,11 +1,12 @@
 package com.powalteam.szopapka;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@Disabled
 class SzopapkaApplicationTests {
 
 	@Test
